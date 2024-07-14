4d5672ce019747b67a01d41c6fa3c0cf33e238dd
ganesh gavali <ganehgavali7090@gmail.com>
2024-07-14 17:50:35 +0530

    USAAir

--- src/test/java/com/example/pro/USAairlines.java
+++ src/test/java/com/example/pro/USAairlines.java
@@ -1,2 +1,11 @@
-package com.example.pro;public class USAairlines {
+package com.example.pro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class USAairlines {
+
+    @GetMapping("/USAairlines")
+    public String getData() {return "United states airways" ; }
 }
